ac363e3e9d0bdd83e7f10bcd8d78e6fd49fdbca4
taehoonkim <rlaxogns4504@gmail.com>
2020-11-24 00:31:02 +0900

    Feature get 1week commit data

--- checker.py
+++ checker.py
@@ -1,11 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
+
+beforeWeek = datetime.date.today() - datetime.timedelta(weeks=1) #일주일 전 날짜
+
 
 response = requests.get('https://github.com/Kim-tang2')
 html = BeautifulSoup(response.content, 'html.parser')
 commitDatas = html.select('rect.day')
 
 for commitData in commitDatas:
-    date = commitData.attrs['data-date']
-    count = commitData.attrs['data-count']
-    print(date, count)
+    if(datetime.datetime.strptime(commitData.attrs['data-date'], "%Y-%m-%d").date() >= beforeWeek):
+        date = commitData.attrs['data-date']
+        count = commitData.attrs['data-count']
+        print(date, count)
+
+
+
