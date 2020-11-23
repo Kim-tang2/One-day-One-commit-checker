d8eee668d783d2a4c0f12736cfb0ab1189e4ca71
taehoonkim <rlaxogns4504@gmail.com>
2020-11-24 00:37:59 +0900

    Feature Day of the week

--- checker.py
+++ checker.py
@@ -2,18 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
+def convertDate(date):
+    return datetime.datetime.strptime(date, "%Y-%m-%d").date()
+
 beforeWeek = datetime.date.today() - datetime.timedelta(weeks=1) #일주일 전 날짜
 
+weeks = ['(월)', '(화)', '(수)', '(목)', '(금)', '(토)', '(일)']
 
 response = requests.get('https://github.com/Kim-tang2')
 html = BeautifulSoup(response.content, 'html.parser')
 commitDatas = html.select('rect.day')
 
 for commitData in commitDatas:
-    if(datetime.datetime.strptime(commitData.attrs['data-date'], "%Y-%m-%d").date() >= beforeWeek):
+    if(convertDate(commitData.attrs['data-date']) >= beforeWeek):
         date = commitData.attrs['data-date']
+        dayOfTheWeek = convertDate(date).weekday()
         count = commitData.attrs['data-count']
-        print(date, count)
+        print(date, weeks[dayOfTheWeek], count)
 
 
 
