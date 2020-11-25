4cc53ca2c131957ba9216bc701210ba3c95b1c24
taehoonkim <rlaxogns4504@gmail.com>
2020-11-25 11:35:18 +0900

    Update print by name as a dictionary

--- checker.py
+++ checker.py
@@ -16,13 +16,20 @@ users = ['Kim-tang2', 'bell2lee', 'kimgyuri', 'sunwoopia',
          'hyeonsang010716', 'youngho-C', 'ddugel3',
          'KMUsungwon', 'kookminju']
 
+usersDic = {'Kim-tang2': '김태훈', 'bell2lee': '이종휘', 'kimgyuri': '김규리',
+            'sunwoopia': '김선우', 'wwjdtm': '김윤정', 'jhg3522': '정현구',
+            'Chord-West': '김현서', 'hyeonn': '임이현', 'hyeonsang010716': '조현상',
+            'youngho-C': '차영호', 'ddugel3': '최건웅', 'KMUsungwon': '임성원',
+            'kookminju': '김민주'
+            }
+
 
 def getCommitData(user):
     response = requests.get('https://github.com/' + user)
     html = BeautifulSoup(response.content, 'html.parser')
     commitDatas = html.select('rect.day')
 
-    print(user, "님의 최근 7일 커밋입니다.")
+    print(usersDic[user] + "님의 최근 7일 커밋입니다.")
     for commitData in commitDatas:
         if convertDate(commitData.attrs['data-date']) >= beforeWeek:
             date = commitData.attrs['data-date']
@@ -34,6 +41,6 @@ def getCommitData(user):
 if __name__ == '__main__':
     startTime = time.time()
     with Pool(processes=4) as pool:
-        pool.map(getCommitData, users)
+        pool.map(getCommitData, usersDic)
 
     print(time.time() - startTime)
